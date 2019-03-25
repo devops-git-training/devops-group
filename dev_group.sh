71b0f69ad627e24e89142fc335a09883654d144b
Govindareddy <sanjeev.govindareddy@capgemini.com>
2019-03-25 12:23:45 +0530

    recent commit

--- dev_group.sh
+++ dev_group.sh
@@ -1,3 +1,10 @@
 Hi Mohan
 
 This is sanjeev. How are you today? I'm fine. Shall we meet for sometime today?
+
+i'm doing devops
+<<<<<<< Updated upstream
+
+Thank you
+=======
+>>>>>>> Stashed changes
